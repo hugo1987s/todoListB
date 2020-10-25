e47cd60a58e93d117c8f3c87f95b1a8d8ce39895
hugo1987s <hugo.schreiber@hasar.com>
2020-10-25 14:38:26 -0300

    initial structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/createTodo.component";
+import EditTodo from "./components/editTodo.component";
+import TodosList from "./components/todos.component";
 
 class App extends Component {
   render() {
     return (
-      
+      <Router>
         <div className="container">
-          <h2>Todo List Test</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">Lista de Tareas - Home</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Tareas</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Crear Tarea</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
         </div>
-      
+      </Router>
     );
   }
 }
---
+++ src/components/createTodo.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateTodo extends Component {
+    render() {
+        return (
+            <div>
+                <p>Creacion de Tarea</p>
+            </div>
+        )
+    }
+}
