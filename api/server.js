e022c7653b4a866d2ac2d7a02a55c063502f50ea
hugo1987s <hugo.schreiber@hasar.com>
2020-10-25 15:18:52 -0300

    Add logs

--- api/server.js
+++ api/server.js
@@ -3,7 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = 1234;
+const todoRoutes = express.Router();
+const PORT = 5000;
+
+let Todo = require('./todo.model');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,6 +17,59 @@ const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("Conexion exitosa a MongoDb");
 })
+
+
+todoRoutes.route('/').get(function(req, res) {
+    console.log("Listando tareas");
+    Todo.find(function(err, todos) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(todos);
+        }
+    });
+});
+
+todoRoutes.route('/:id').get(function(req, res) {
+    let id = req.params.id;
+    Todo.findById(id, function(err, todo) {
+        res.json(todo);
+    });
+});
+
+todoRoutes.route('/update/:id').post(function(req, res) {
+    Todo.findById(req.params.id, function(err, todo) {
+        if (!todo)
+            res.status(404).send("sin información");
+        else
+            todo.todo_description = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+
+            todo.save().then(todo => {
+                res.json('Tarea actualizada');
+            })
+            .catch(err => {
+                res.status(400).send("Error al actualizar");
+            });
+    });
+});
+
+todoRoutes.route('/add').post(function(req, res) {
+    console.log("agregando tarea");
+    let todo = new Todo(req.body);
+    todo.save()
+        .then(todo => {
+            res.status(200).json({'todo': 'Tarea agregada'});
+        })
+        .catch(err => {
+            res.status(400).send('Fallo al crear la tarea');
+        });
+});
+
+app.use('/todos', todoRoutes);
+
 
 app.listen(PORT, function() {
     console.log("Server corriendo en el puerto: " + PORT);
